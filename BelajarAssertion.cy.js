e363d4aa40b8f186cbbd93ea8f993043b70c95da
yusuf19p <130345956+yusuf19p@users.noreply.github.com>
2023-05-14 11:58:14 +0700

    Add files via upload

---
+++ BelajarAssertion.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('clicking "type" shows the right headings', () => {
+        cy.visit('https://example.cypress.io')
+        cy.pause()
+        cy.contains('type').click()
+        cy.url().should('include', '/commands/actions')
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
+    });
+});
