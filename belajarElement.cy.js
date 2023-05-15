48657548cbf75d0c61dce8bf6441fdad048dccb2
yusuf19p <130345956+yusuf19p@users.noreply.github.com>
2023-05-15 13:49:07 +0700

    Add files via upload

---
+++ belajarElement.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Working with input',{ testIsolation: false },() => {
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+    it('Should fill checkbox', () => {                       
+        cy.get('#user_remember_me').check()
+        cy.get('#user_remember_me').type('checkbox')
+    });
+});
