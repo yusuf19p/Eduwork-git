c7083658fa2c727623148b3ae9171cf3383be3df
yusuf19p <130345956+yusuf19p@users.noreply.github.com>
2023-06-03 21:45:17 +0700

    belajar searchbox.test.cy.js

--- cypress/e2e/0-personal learn/integration/belajar searchbox.test.cy.js
+++ cypress/e2e/0-personal learn/integration/belajar searchbox.test.cy.js
@@ -9,13 +9,18 @@ describe('Searchbox Test',{ testIsolation : false }, function() {
         cy.get('#searchTerm').type('online {enter}')
     });
 
-    it('Should type into searchbox and submit', () => {
+    it('Should display the first result', () => {
         cy.get('a[href*="/online-banking.html"]').should('contain.text', 'Zero - Free Access to Online Banking')
     });
 
-    it('Should type into searchbox and submit', () => {
+    it('Should display the second result', () => {
         cy.get('a[href*="/bank/online-statements.html"]').should('contain.text', 'Zero - Online Statements')
     });
-
+    
+    it('Should display online banking content', () => {
+        cy.get('a[href*="/online-banking.html"]').click()
+        cy.get('h1').should('be.visible')
+        cy.url().should('include', 'online-banking.html')
+    });
 });
 
