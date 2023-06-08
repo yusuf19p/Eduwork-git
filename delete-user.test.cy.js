e4722aaf7421f335815ed6bbab7d3f0d6de3d41e
yusuf19p <130345956+yusuf19p@users.noreply.github.com>
2023-06-09 01:27:43 +0700

    Delete delete-user.test.cy.js

--- delete-user.test.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Delete User', () => {
-    it('Succesfully delete user', () => {
-
-    cy.request('DELETE', 'https://reqres.in/api/users/2').then((response) =>{
-        expect(response.status).equal(204)
-    })
-    });
-});
