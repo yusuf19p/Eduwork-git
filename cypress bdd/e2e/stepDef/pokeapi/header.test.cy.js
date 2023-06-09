2cb985bdd27f4fbe43a98c28cde7d7fc8b85796e
yusuf19p <130345956+yusuf19p@users.noreply.github.com>
2023-06-09 22:40:01 +0700

    Update header.test.cy.js

--- cypress bdd/e2e/stepDef/pokeapi/header.test.cy.js
+++ cypress bdd/e2e/stepDef/pokeapi/header.test.cy.js
@@ -1,14 +1,18 @@
+///<reference types="cypress" />
 describe('Validate Header and Response Body', () => {
 
-    it('Successfully validate content-type', () => {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('content-type')
-        .should('include', 'application/json; charset=utf-8')
+  it('Successfully validate content-type', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+    cy.get('@pokemon').its('headers').its('content-type')
+      .should('include', 'application/json; charset=utf-8')
+  });
 
-        cy.get('@pokemon').its('body').then((responseBody) => {
-        expect(responseBody).to.have.property('name', 'ditto')
-        expect(responseBody).to.have.property('height').that.is.a('number')
-        expect(responseBody).to.have.property('weight').that.is.a('number')
-      })
-    })
-})
+  it('Successfully validate response body', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')  
+    cy.get('@pokemon').its('body').then((responseBody) => {
+      expect(responseBody).to.have.property('name', 'ditto')
+      expect(responseBody).to.have.property('height').that.is.a('number')
+      expect(responseBody).to.have.property('weight').that.is.a('number')
+      });
+  });
+});
